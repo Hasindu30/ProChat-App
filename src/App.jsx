58e6abb8cb80e813f262bddbe258fe5eba622376
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-09-03 02:13:31 +0530

    update the login sign up pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Chat from './pages/Chat/Chat'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
+import Login from './pages/Login/Login'
 
 const App = () => {
   return (
-    <div></div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/profile' element={<ProfileUpdate/>}/>
+
+      </Routes>
+
+    </>
   )
 }
 
