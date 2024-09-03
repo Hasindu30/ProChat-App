d809d43c606a2f1d31663da820ea700434a6205a
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-09-03 09:55:47 +0530

    update the rightside

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
 import './ChatBox.css'
+import assets from '../../assets/assets';
 
 const ChatBox = () => {
   return (
-    <div>
-      
+    <div className='chat-box'>
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="User Profile" />
+        <p>Richard Sanford <img className='dot' src={assets.green_dot} alt="Online Status" /></p>
+        <img src={assets.help_icon} className='help' alt="Help Icon" />
+      </div>
+
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">
+            lorem ipsum is placeholder text only,,,,
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="s-msg">
+          <img className='msg-img' src={assets.pic1} alt="" />
+          <div>
+            <img  src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">
+            lorem ipsum is placeholder text only,,,,
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="chat-input">
+        <input type="text" placeholder='Send a message' />
+        <input type="file" id='image' accept='image/png, image/jpeg' hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="Upload Image" />
+        </label>
+        <img src={assets.send_button} alt="Send Message" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ChatBox
