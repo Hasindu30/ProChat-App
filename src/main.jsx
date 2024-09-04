40e5d0d533d6e1bd57cb2a4eb2edd4655acccd32
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-09-04 13:06:43 +0530

    update the authentification

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' 
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <App />
+  <AppContextProvider>
+    <App />
+  </AppContextProvider>
+  
   </BrowserRouter>
     
   
