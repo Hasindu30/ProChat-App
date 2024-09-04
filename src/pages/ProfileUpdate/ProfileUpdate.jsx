172ccea75021b296c3606eaf7eba9ed88d59a67f
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-09-05 01:48:46 +0530

    update the login sign up finish

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './ProfileUpdate.css'
 import assets from '../../assets/assets'
 import { onAuthStateChanged } from 'firebase/auth';
@@ -7,6 +7,7 @@ import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import upload from '../../lib/upload';
+import { AppContext } from '../../context/AppContext';
 
 const ProfileUpdate = () => {
   const navigate = useNavigate();
@@ -14,7 +15,8 @@ const ProfileUpdate = () => {
     const [name,setName] = useState("");
     const [bio,setBio] = useState("");
     const [uid,setUid] =useState("");
-    const[prevImage,setPrevImage] = useState("")
+    const[prevImage,setPrevImage] = useState("");
+    const {setUserData} = useContext(AppContext)
 
     const ProfileUpdate = async (event) => {
         event.preventDefault();
@@ -41,8 +43,12 @@ const ProfileUpdate = () => {
             name:name
           })
         }
+        const snap = await getDoc(docRef);
+        setUserData(snap.data());
+        navigate('/chat');
        } catch (error) {
-         
+         console.error(error);
+         toast.error(error.message);
        }
     }
 
@@ -83,7 +89,7 @@ const ProfileUpdate = () => {
                     <textarea onChange={(e)=>setBio(e.target.bio)} value={bio} placeholder='Write profile bio' required></textarea>
                     <button type='submit'>Save</button>
                 </form>
-                <img className='profile-pic' src={image? URL.createObjectURL(image) : assets.logo_icon} alt="" />
+                <img className='profile-pic' src={image? URL.createObjectURL(image) :prevImage ? prevImage : assets.logo_icon} alt="" />
             </div>
     </div>
   )
