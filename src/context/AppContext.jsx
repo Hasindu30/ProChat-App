0119321d6123a5521379e87862d995ee9e11dbb9
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-09-06 12:33:29 +0530

    update and chat page

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -42,7 +42,6 @@ if (userData) {
     const chatRef = doc(db,'chats',userData.id);
     const unSub = onSnapshot(chatRef,async (res)=>{
         const chatItems = res.data().chatsData;
-        console.log(chatItems());
         const tempData = [];
         for(const item of chatItems ){
             const userRef = doc(db,'users',item.rId);
